601c1706d66e39a4c750aceca3264aa8eb061c6d
manh tam <manhtam96@gmail.com>
2024-01-14 22:20:53 +0700

    Homepage

--- lib/widgets/homepage.dart
+++ lib/widgets/homepage.dart
@@ -1,13 +1,16 @@
+import 'package:setting_tab/widgets/settings.dart';
+
 import '/widgets/components/my_drawer.dart';
 import '/models/user_interface.dart';
 import 'package:flutter/material.dart';
 import "package:provider/provider.dart";
-List catNames = ["Sinh viên", 'Cài đặt'];
+List imgList = ["Sinh viên", 'Cài đặt'];
 List<Color> catColors = [
   Color(0xFFFFCF2F),
   Color(0xFF6Fe08D),
 
 ];
+
 
 List<Icon> catIcons = [
   Icon(Icons.manage_accounts_outlined, color: Colors.white, size: 30),
@@ -55,39 +58,89 @@ class MyHomePage extends StatelessWidget {
               child: Column(
                 children: [
                   GridView.builder(
-                    itemCount: catNames.length,
+                    itemCount: imgList.length,
                     shrinkWrap: true,
-                      physics: NeverScrollableScrollPhysics(),
-                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 3,
-                        childAspectRatio: 1.1,
-                      ),
+                    physics: NeverScrollableScrollPhysics(),
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      childAspectRatio: (MediaQuery.of(context).size.height - 50 - 25) / (4 * 240),
+                      mainAxisSpacing: 10,
+                      crossAxisSpacing: 10,
+                    ),
                     itemBuilder: (context, index){
-                      return Column(
-                        children: [
-                          Container(
-                            height: 60,
-                            width: 60,
-                            decoration: BoxDecoration(
-                              color: catColors[index],
-                              shape: BoxShape.circle,
-                            ),
-                            child: Center(
-                              child: catIcons[index],
-                            ),
+                      return InkWell(
+                        onTap: (){
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => MySettings(),
+                            )
+                          );
+                        },
+                        child: Container(
+                          padding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Colors.white,
                           ),
-                          SizedBox(height: 10),
-                          Text(
-                            catNames[index],
-                            style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.w500,
-                              color: Colors.black.withOpacity(0.7),
-                            ),
+                          child: Column(
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.all(10),
+                                child: Image.asset(
+                                  "assests/icons/${imgList[index]}.png",
+                                  width: 60,
+                                  height: 60,
+                                ),
+                              ),
+                              SizedBox(height: 10),
+                              Text(
+                                  imgList[index],
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.black.withOpacity(0.7),
+                                ),
+                              )
+                            ],
                           ),
-                        ],
+                        ),
+
                       );
                     },
+                    // itemCount: catNames.length,
+                    // shrinkWrap: true,
+                    //   physics: NeverScrollableScrollPhysics(),
+                    //   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    //     crossAxisCount: 3,
+                    //     childAspectRatio: 1.1,
+                    //   ),
+                    // itemBuilder: (context, index){
+                    //   return Column(
+                    //     children: [
+                    //       Container(
+                    //         height: 60,
+                    //         width: 60,
+                    //         decoration: BoxDecoration(
+                    //           color: catColors[index],
+                    //           shape: BoxShape.circle,
+                    //         ),
+                    //         child: Center(
+                    //           child: catIcons[index],
+                    //         ),
+                    //       ),
+                    //       SizedBox(height: 10),
+                    //       Text(
+                    //         catNames[index],
+                    //         style: TextStyle(
+                    //           fontSize: 16,
+                    //           fontWeight: FontWeight.w500,
+                    //           color: Colors.black.withOpacity(0.7),
+                    //         ),
+                    //       ),
+                    //     ],
+                    //   );
+                    // },
                   ),
                 ],
               ),
