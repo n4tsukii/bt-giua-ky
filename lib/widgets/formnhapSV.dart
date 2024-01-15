1af1548486ac29a60dca8729540c19a1620eb980
m1soshiru <73549301+m1soshiru@users.noreply.github.com>
2024-01-15 08:04:41 +0700

    add danhsachSV,formnhapSV

---
+++ lib/widgets/formnhapSV.dart
@@ -0,0 +1,64 @@
+import "package:flutter/material.dart";
+
+class FormNhapSinhVien extends StatelessWidget {
+  final Function addSinhVien;
+
+  final maController = TextEditingController();
+  final hoVaTenController = TextEditingController();
+  final diemTotNghiepController = TextEditingController();
+
+  FormNhapSinhVien(this.addSinhVien);
+
+  submitData() {
+    final enterMaController = maController.text;
+    final enterHoVaTenController = hoVaTenController.text;
+    final enterDiemController = diemTotNghiepController.text;
+
+    if (enterHoVaTenController.length > 3)
+      addSinhVien(
+        int.parse(enterMaController),
+        enterHoVaTenController,
+        double.parse(enterDiemController),
+      );
+    else
+      return;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            TextField(
+              decoration: InputDecoration(labelText: 'Ma sinh vien'),
+              controller: maController,
+              onSubmitted: (_) => submitData(),
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Ho va ten'),
+              controller: hoVaTenController,
+              onSubmitted: (_) => submitData(),
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Diem tot nghiep'),
+              controller: diemTotNghiepController,
+              onSubmitted: (_) => submitData(),
+            ),
+            TextButton(
+              child: Text('Them sinh vien'),
+              onPressed: (){
+                submitData();
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
