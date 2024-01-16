e24968b3bbdd7840593b3b79c3c53067e2d950fa
manh tam <manhtam96@gmail.com>
2024-01-16 21:35:19 +0700

    chỉnh sửa giao diện homepage

--- lib/widgets/homepage.dart
+++ lib/widgets/homepage.dart
@@ -1,131 +1,205 @@
 import 'package:setting_tab/widgets/ql_sinhvien.dart';
 import 'package:setting_tab/widgets/settings.dart';
-import '/widgets/components/my_drawer.dart';
-import '/models/user_interface.dart';
 import 'package:flutter/material.dart';
-import "package:provider/provider.dart";
 List imgList = ['Sinh viên', 'Môn học', 'Cài đặt'];
 
 class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: ListView(
+        padding: EdgeInsets.zero,
         children: [
           Container(
-            padding: EdgeInsets.only(top: 15, left: 15, right: 15, bottom: 10),
+            // padding: EdgeInsets.only(top: 15, left: 15, right: 15, bottom: 10),
             decoration: const BoxDecoration(
               color: Colors.blueAccent,
               borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(20),
-                bottomRight: Radius.circular(20),
+                bottomRight: Radius.circular(50),
               )
             ),
-            child: const Column(
+            child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                SizedBox(height: 20),
-                Padding(
-                  padding: EdgeInsets.only(left: 3, bottom: 15),
-                  child: Text(
-                      'Quản lý sinh viên',
-                    style: TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.w600,
-                      letterSpacing: 1,
-                      wordSpacing: 2,
-                      color: Colors.white,
-                    ),
+                const SizedBox(height: 50),
+                ListTile(
+                  contentPadding: const EdgeInsets.symmetric(horizontal: 30),
+                  title: Text(
+                    'Quản lý sinh viên',
+                    style: Theme.of(context).textTheme.headlineSmall?.copyWith(color: Colors.white)
+                  ),
+                  trailing: const CircleAvatar(
+                    radius: 30,
+                    backgroundImage: AssetImage('assests/icons/internship.png'),
                   ),
                 ),
+                const SizedBox(height: 30),
+                // Padding(
+                //   padding: EdgeInsets.only(left: 3, bottom: 15),
+                //   child: Text(
+                //       'Quản lý sinh viên',
+                //     style: TextStyle(
+                //       fontSize: 25,
+                //       fontWeight: FontWeight.w600,
+                //       letterSpacing: 1,
+                //       wordSpacing: 2,
+                //       color: Colors.white,
+                //     ),
+                //   ),
+                // ),
               ],
             ),
           ),
-          Padding(
-              padding: EdgeInsets.only(top: 20, left: 15, right: 15),
-              child: Column(
-                children: [
-                  GridView.builder(
-                    itemCount: imgList.length,
-                    shrinkWrap: true,
-                    physics: NeverScrollableScrollPhysics(),
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                      childAspectRatio: (MediaQuery.of(context).size.height - 50 - 25) / (4 * 240),
-                      mainAxisSpacing: 10,
-                      crossAxisSpacing: 10,
-                    ),
-                    itemBuilder: (context, index){
-                      return GestureDetector(
-                        onTap: () {
-                          handleNavigation(context, index);
-                        },
-                        child: Container(
-                          padding: EdgeInsets.symmetric(vertical: 10, horizontal: 5),
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20),
-                            color: Colors.white,
-                          ),
-                          child: Column(
-                            children: [
-                              Padding(
-                                padding: EdgeInsets.all(10),
-                                child: Image.asset(
-                                  "assests/icons/${imgList[index]}.png",
-                                  width: 60,
-                                  height: 60,
-                                ),
-                              ),
-                              SizedBox(height: 10),
-                              Text(
-                                  imgList[index],
-                                style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w500,
-                                  color: Colors.black.withOpacity(0.7),
-                                ),
-                              )
-                            ],
-                          ),
-                        ),
-                      );
-                    },
-                    // itemCount: catNames.length,
-                    // shrinkWrap: true,
-                    //   physics: NeverScrollableScrollPhysics(),
-                    //   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    //     crossAxisCount: 3,
-                    //     childAspectRatio: 1.1,
-                    //   ),
-                    // itemBuilder: (context, index){
-                    //   return Column(
-                    //     children: [
-                    //       Container(
-                    //         height: 60,
-                    //         width: 60,
-                    //         decoration: BoxDecoration(
-                    //           color: catColors[index],
-                    //           shape: BoxShape.circle,
-                    //         ),
-                    //         child: Center(
-                    //           child: catIcons[index],
-                    //         ),
-                    //       ),
-                    //       SizedBox(height: 10),
-                    //       Text(
-                    //         catNames[index],
-                    //         style: TextStyle(
-                    //           fontSize: 16,
-                    //           fontWeight: FontWeight.w500,
-                    //           color: Colors.black.withOpacity(0.7),
-                    //         ),
-                    //       ),
-                    //     ],
-                    //   );
-                    // },
+          Container(
+            color: Colors.blueAccent,
+              // padding: EdgeInsets.only(top: 20, left: 15, right: 15),
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(100)
+                  )
+                ),
+                child: GridView.builder(
+                  itemCount: imgList.length,
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    childAspectRatio: 1,
                   ),
-                ],
+                  itemBuilder: (context, index){
+                    return GestureDetector(
+                      onTap: () {
+                        handleNavigation(context, index);
+                      },
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(vertical: 10),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(
+                              offset: const Offset(0, 5),
+                              color: Theme.of(context).primaryColor.withOpacity(.2),
+                              spreadRadius: 2,
+                              blurRadius: 5,
+                            )
+                          ]
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Padding(
+                              padding: EdgeInsets.all(10),
+                              child: Image.asset(
+                                "assests/icons/${imgList[index]}.png",
+                                width: 70,
+                                height: 70,
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                            Text(
+                              imgList[index],
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500,
+                                color: Colors.black.withOpacity(0.7),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                ),
               ),
+
+
+              // child: Column(
+              //   children: [
+              //     GridView.builder(
+              //       itemCount: imgList.length,
+              //       shrinkWrap: true,
+              //       physics: NeverScrollableScrollPhysics(),
+              //       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              //         crossAxisCount: 2,
+              //         childAspectRatio: 0.9,
+              //       ),
+              //       itemBuilder: (context, index){
+              //         return GestureDetector(
+              //           onTap: () {
+              //             handleNavigation(context, index);
+              //           },
+              //           child: Container(
+              //             padding: EdgeInsets.symmetric(vertical: 40),
+              //             decoration: BoxDecoration(
+              //               borderRadius: BorderRadius.circular(20),
+              //               color: Colors.white,
+              //             ),
+              //             child: Column(
+              //               children: [
+              //                 Padding(
+              //                   padding: EdgeInsets.all(10),
+              //                   child: Image.asset(
+              //                     "assests/icons/${imgList[index]}.png",
+              //                     width: 60,
+              //                     height: 60,
+              //                   ),
+              //                 ),
+              //                 SizedBox(height: 10),
+              //                 Text(
+              //                     imgList[index],
+              //                   style: TextStyle(
+              //                     fontSize: 16,
+              //                     fontWeight: FontWeight.w500,
+              //                     color: Colors.black.withOpacity(0.7),
+              //                   ),
+              //                 )
+              //               ],
+              //             ),
+              //           ),
+              //         );
+              //       },
+              //       // itemCount: catNames.length,
+              //       // shrinkWrap: true,
+              //       //   physics: NeverScrollableScrollPhysics(),
+              //       //   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              //       //     crossAxisCount: 3,
+              //       //     childAspectRatio: 1.1,
+              //       //   ),
+              //       // itemBuilder: (context, index){
+              //       //   return Column(
+              //       //     children: [
+              //       //       Container(
+              //       //         height: 60,
+              //       //         width: 60,
+              //       //         decoration: BoxDecoration(
+              //       //           color: catColors[index],
+              //       //           shape: BoxShape.circle,
+              //       //         ),
+              //       //         child: Center(
+              //       //           child: catIcons[index],
+              //       //         ),
+              //       //       ),
+              //       //       SizedBox(height: 10),
+              //       //       Text(
+              //       //         catNames[index],
+              //       //         style: TextStyle(
+              //       //           fontSize: 16,
+              //       //           fontWeight: FontWeight.w500,
+              //       //           color: Colors.black.withOpacity(0.7),
+              //       //         ),
+              //       //       ),
+              //       //     ],
+              //       //   );
+              //       // },
+              //     ),
+              //   ],
+              // ),
           ),
         ],
       ),
