1e2fd13a6699dc1bb2fd14122631fb4a0459e410
manh tam <manhtam96@gmail.com>
2024-01-19 17:27:30 +0700

    update

--- lib/widgets/sinhvien/display_listSV.dart
+++ lib/widgets/sinhvien/display_listSV.dart
@@ -1,101 +1,178 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../../models/sinhvien.dart';
-
 class DanhSachSinhVien extends StatelessWidget {
   final List<SinhVien> danhSachSinhVien;
-
   DanhSachSinhVien(this.danhSachSinhVien);
-
   @override
   Widget build(BuildContext context) {
-    return Consumer<StudentProvider>(
-      builder: (context, studentProvider, child) {
-        if (studentProvider.currentStudent.isNotEmpty) {
-          return Column(
-            children: studentProvider.currentStudent.map((student) {
-              return Card(
-                child: ListTile(
-                  title: Row(
-                    children: [
-                      Container(
-                        margin: const EdgeInsets.symmetric(
-                          vertical: 10,
-                          horizontal: 10,
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Consumer<StudentProvider>(
+            builder: (context, studentProvider, child) {
+              if (studentProvider.currentStudent.isNotEmpty) {
+                return Column(
+                  children: studentProvider.currentStudent.map((student) {
+                    return Card(
+                      child: ListTile(
+                        title: Row(
+                          children: [
+                            Container(
+                              margin: const EdgeInsets.symmetric(
+                                vertical: 10,
+                                horizontal: 10,
+                              ),
+                              decoration: BoxDecoration(
+                                  border: Border.all(
+                                    color: Colors.purple,
+                                    width: 2,
+                                  )),
+                              padding: const EdgeInsets.all(10),
+                              child: Text(student.diem.toString(),
+                                  style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 18,
+                                    color: Colors.purple,
+                                  )),
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Mã sinh viên: ${student.maSV}',
+                                  style: const TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  'Họ tên: ${student.hoten}',
+                                  style: const TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  "Lớp: ${student.lop}",
+                                  style: const TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
                         ),
-                        decoration: BoxDecoration(
-                          border: Border.all(
-                            color: Colors.purple,
-                            width: 2,
-                          ),
-                        ),
-                        padding: const EdgeInsets.all(10),
-                        child: Text(
-                          student.diem.toString(),
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Colors.purple,
-                          ),
+                        trailing: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            IconButton(
+                              icon: Icon(Icons.delete_forever),
+                              onPressed: (){
+                                _deleteStudent(context, student);
+                              },
+                            )
+                          ],
                         ),
                       ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Mã sinh viên: ${student.maSV}',
-                            style: const TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Text(
-                            'Họ tên: ${student.hoten}',
-                            style: const TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          Text(
-                            "Lớp: ${student.lop}",
-                            style: const TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
+                    );
+                  }).toList(),
+                );
+              } else {
+                return const Text(
+                  'Chưa thêm sinh viên nào',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
                   ),
-                  trailing: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      IconButton(
-                        icon: Icon(Icons.delete),
-                        onPressed: () {
-                          _deleteStudent(context, student);
-                        },
-                      ),
-                    ],
-                  ),
-                ),
-              );
-            }).toList(),
-          );
-        } else {
-          return const Text(
-            'Chưa thêm sinh viên nào',
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.bold,
-            ),
-          );
-        }
-      },
+                );
+              }
+            },
+          ),
+        ],
+      ),
     );
   }
-
   void _deleteStudent(BuildContext context, SinhVien student) {
     Provider.of<StudentProvider>(context, listen: false).deleteStudent(student);
   }
 }
+// class DanhSachSinhVien extends StatelessWidget {
+//   final List<SinhVien> danhSachSinhVien;
+//   DanhSachSinhVien(this.danhSachSinhVien);
+//   @override
+//   Widget build(BuildContext context) {
+//     return SingleChildScrollView(
+//       child: Column(
+//         children: [
+//           Consumer<StudentProvider>(
+//             builder: (context, studentProvider, child) {
+//               if (studentProvider.currentStudent.isNotEmpty) {
+//                 return Column(
+//                   children: studentProvider.currentStudent.map((student) {
+//                     return Card(
+//                       child: Row(
+//                         mainAxisAlignment: MainAxisAlignment.start,
+//                         children: <Widget>[
+//                           Container(
+//                             margin: const EdgeInsets.symmetric(
+//                               vertical: 10,
+//                               horizontal: 15,
+//                             ),
+//                             decoration: BoxDecoration(
+//                                 border: Border.all(
+//                                   color: Colors.purple,
+//                                   width: 2,
+//                                 )),
+//                             padding: const EdgeInsets.all(10),
+//                             child: Text(student.diem.toString(),
+//                                 style: const TextStyle(
+//                                   fontWeight: FontWeight.bold,
+//                                   fontSize: 20,
+//                                   color: Colors.purple,
+//                                 )),
+//                           ),
+//                           Column(
+//                             crossAxisAlignment: CrossAxisAlignment.start,
+//                             children: [
+//                               Text(
+//                                 'Mã sinh viên: ${student.maSV}',
+//                                 style: const TextStyle(
+//                                     fontSize: 16,
+//                                     fontWeight: FontWeight.bold),
+//                               ),
+//                               Text(
+//                                 'Họ tên: ${student.hoten}',
+//                                 style: const TextStyle(
+//                                     fontSize: 16,
+//                                     fontWeight: FontWeight.bold),
+//                               ),
+//                               Text(
+//                                 "Lớp: ${student.lop}",
+//                                 style: const TextStyle(
+//                                     fontSize: 16,
+//                                     fontWeight: FontWeight.bold
+//                                 ),
+//                               ),
+//                             ],
+//                           ),
+//                         ],
+//                       ),
+//                     );
+//                   }).toList(),
+//                 );
+//               } else {
+//                 return const Text(
+//                   'Chưa thêm sinh viên nào',
+//                   style: TextStyle(
+//                     fontSize: 16,
+//                     fontWeight: FontWeight.bold,
+//                   ),
+//                 );
+//               }
+//             },
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
