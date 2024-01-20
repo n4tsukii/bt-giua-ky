25ce984a5d17a488bf584a305d58ef2f97ca486f
manh tam <manhtam96@gmail.com>
2024-01-20 15:01:42 +0700

    thêm chức năng nhập điểm

--- lib/models/sinhvien.dart
+++ lib/models/sinhvien.dart
@@ -1,25 +1,28 @@
 import 'package:flutter/foundation.dart';
+import '../models/monhoc.dart';
 
 class SinhVien {
-  final int maSV;
-  final String hoten;
+  late final int maSV;
+  late final String hoten;
   late final double diem;
-  final String lop;
+  late final String lop;
 
   SinhVien({
     required this.maSV,
     required this.hoten,
     required this.diem,
     required this.lop,
+
   });
 }
 
 class StudentProvider with ChangeNotifier {
+  double threshold = 0.0;
   List<SinhVien> dsSV = [
     SinhVien(maSV: 22010498, hoten: "Pham Nhu Thuat", diem: 2, lop: "K16"),
     SinhVien(maSV: 21010651, hoten: "Manh Tam", diem: 3, lop: "K15"),
   ];
-  List<SinhVien> get currentStudent => dsSV;
+  List<SinhVien> get currentStudent => filterStudents(threshold);
 
   void addStudent(int maSV, String hoten, double diem, String lop) {
     SinhVien newStudent = SinhVien(maSV: maSV, hoten: hoten, diem: diem, lop: lop);
@@ -32,8 +35,14 @@ class StudentProvider with ChangeNotifier {
     notifyListeners();
   }
 
-  void filterStudentsByAverage(double threshold) {
-    dsSV = dsSV.where((student) => student.diem > threshold).toList();
+
+  void updatethreshold(double threshold) {
+    this.threshold = threshold;
     notifyListeners();
   }
+
+  List<SinhVien> filterStudents(double threshold) {
+    return dsSV.where((student) => student.diem > threshold).toList();
+  }
+
 }
