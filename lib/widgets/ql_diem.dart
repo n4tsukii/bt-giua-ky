1821bd7158c7b3b1f874275af4f14c7300dd66e2
m1soshiru <73549301+m1soshiru@users.noreply.github.com>
2024-01-22 07:47:37 +0700

    .

--- lib/widgets/ql_diem.dart
+++ lib/widgets/ql_diem.dart
@@ -247,9 +247,9 @@ class _QLDiem extends State<QLDiem> {
       }
       if (studentCount > 0) {
         subjectAverage /= studentCount;
-        return Text('Điểm môn $selectedSubject: ${subjectAverage.toStringAsFixed(2)}');
+        return Text('');
       } else {
-        return Text('Sinh viên chưa có điểm môn $selectedSubject');
+        return Text('');
       }
     }
     return Container();
